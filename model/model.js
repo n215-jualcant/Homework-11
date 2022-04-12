8c81182b808b107dc7c076670639c177fca845a0
JuanAlcantarJr <j.c.alcantar2002@gmail.com>
2022-04-12 10:33:12 -0400

    code

--- model/model.js
+++ model/model.js
@@ -14,7 +14,21 @@ export var myName = "todd";
 
 // page name being pulled from app.js
 export function modelPageName(pgName){
-    console.log("pgName");
+    // console.log("pgName");
 
+    // $("#app").html(eval(pgName));
+    if(pgName == "homeContent") {
+        $("nav a").removeClass("blue-text").addClass("white-text");
+        $("nav span").removeClass("blue-text").addClass("white-text");
+
+} else{
+    $("nav a").removeClass("white-text").addClass("blue-text");
+    $("nav span").removeClass("white-text").addClass("blue-text");
+}
+try{
+    eval(pgName);
     $("#app").html(eval(pgName));
+} catch (e){
+    console.log("hello", e);
+}
 }
